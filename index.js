b232fe33a4b3a683530b72e700e17b6674ac730c
haydee <haydeemunozdelarocha@gmail.com>
2018-12-07 21:05:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/', (req, res) => {
+  console.log(`App running`, req, res);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`App is listening on ${port}`);
